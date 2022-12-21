96af1e19068eb598094180ef22b6d6e13a0873f2
José Adriano <adriano.liukin@alu.ufc.br>
2022-12-21 10:12:37 -0300

    Atualização

--- ExercitandoAPI/Program.cs
+++ ExercitandoAPI/Program.cs
@@ -1,9 +1,23 @@
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+//app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/produtos/{Id}", ( [FromRoute] int Id) =>
+{
+    var produto = ProdutoRepositorio.GetBy(Id);
+    if (produto != null)
+    {
+        return Results.Ok(produto);
+    }
+    else
+    {
+        return Results.NotFound();
+    }
+});
 
 app.Run();
 
@@ -15,6 +29,24 @@ public static class ProdutoRepositorio
     {
         var produtos = configuracao.GetSection("Produtos").Get<List<Produto>>();
         Produtos = produtos;
+    }
+
+    public static void Add(Produto produto)
+    {
+        if (Produtos == null)
+            Produtos = new List<Produto>();             //Inicialização da lista para armazenamento dos produtos
+
+        Produtos.Add(produto);                          //Adicionando produto a nossa lista
+    }
+
+    public static Produto GetBy(int Id)
+    {
+        return Produtos.FirstOrDefault(p => p.Id == Id);
+    }
+
+    public static void Remove(Produto produto)
+    {
+        Produtos.Remove(produto);                          //Removendo produto de nossa lista
     }
 }
 
